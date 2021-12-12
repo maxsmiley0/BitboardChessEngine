482634ac3fb9bbeceaeaa7d1d7694d6703854022
Max Smiley <maxsmiley0@gmail.com>
2021-12-12 15:03:49 -0800

    Implemented is_square_attacked, and a routine to print all attacked squares.

--- bitboard_chess_engine/bbc.c
+++ bitboard_chess_engine/bbc.c
@@ -213,11 +213,11 @@ void print_board()
                 }
             }
 
-            printf(" %s", (piece == -1) ? "." : unicode_pieces[piece]);
+            printf(" %c", (piece == -1) ? '.' : ascii_pieces[piece]);
         }
         printf("\n");
     }
-    printf("\n   a b c d e f g h\n\n");
+    printf("\n    a b c d e f g h\n\n");
 
     printf("       Side:     %s\n", !side ? "white" : "black");
 
@@ -976,17 +976,48 @@ void init_all()
     init_sliders_attacks(rook);
 }
 
+//Is square attacked given a side?
+static inline int is_square_attacked(int square, int side)
+{
+    //If there is a white pawn in the bottom left / bottom right sqs
+    if ((side == white) && (pawn_attacks[black][square] & bitboards[P])) return 1;
+    if ((side == black) && (pawn_attacks[white][square] & bitboards[p])) return 1;
+    
+    //If knights are on any of the knight attacking squares
+    if (knight_attacks[square] & ((side == white) ? bitboards[N] : bitboards[n])) return 1;
+    if (king_attacks[square] & ((side == white) ? bitboards[K] : bitboards[k])) return 1;
+
+    if (get_bishop_attacks(square, occupancies[both]) & ((side == white) ? bitboards[B] : bitboards[b])) return 1;
+    if (get_rook_attacks(square, occupancies[both]) & ((side == white) ? bitboards[R] : bitboards[r])) return 1;
+    if (get_queen_attacks(square, occupancies[both]) & ((side == white) ? bitboards[Q] : bitboards[q])) return 1;
+    return 0;
+}
+
+void print_attacked_squares(int side)
+{
+    for (int rank = 0; rank < 8; rank++)
+    {
+        for (int file = 0; file < 8; file++)
+        {
+            int square = rank * 8 + file;
+            if (!file)
+            {
+                printf("  %d ", 8 - rank);
+            }
+            printf("%d ", is_square_attacked(square, side) ? 1 : 0);
+        }
+        printf("\n");
+    }
+    printf("    a b c d e f g h\n\n");
+}
+
 //Main driver
 int main()
 {
     init_all();
-    U64 occupancy = 0ULL;
-    set_bit(occupancy, b6);
-    set_bit(occupancy, d6);
-    set_bit(occupancy, f6);
-    set_bit(occupancy, b4);
-    set_bit(occupancy, g4);
-    print_bitboard(get_queen_attacks(d4, occupancy));
+    parse_fen(tricky_position);
     print_board();
+    print_bitboard(occupancies[both]);
+    print_attacked_squares(white);
     return 0;
 }
