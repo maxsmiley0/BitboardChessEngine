2c5a6c1c034413c943c72ff849878d01791f6354
Max Smiley <maxsmiley0@gmail.com>
2021-12-12 23:19:24 -0800

    Defined move encoding scheme

--- bitboard_chess_engine/bbc.c
+++ bitboard_chess_engine/bbc.c
@@ -1372,6 +1372,19 @@ static inline void generate_moves()
     }
 }
 
+/*
+          binary move bits                               hexidecimal constants
+    
+    0000 0000 0000 0000 0011 1111    source square       0x3f
+    0000 0000 0000 1111 1100 0000    target square       0xfc0
+    0000 0000 1111 0000 0000 0000    piece               0xf000
+    0000 1111 0000 0000 0000 0000    promoted piece      0xf0000
+    0001 0000 0000 0000 0000 0000    capture flag        0x100000
+    0010 0000 0000 0000 0000 0000    double push flag    0x200000
+    0100 0000 0000 0000 0000 0000    enpassant flag      0x400000
+    1000 0000 0000 0000 0000 0000    castling flag       0x800000
+*/
+
 //Main driver
 int main()
 {
